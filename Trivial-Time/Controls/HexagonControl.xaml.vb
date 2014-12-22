d5b3200b3e889d579d359ac55f3acbbf136e8ae6
Killian Meersman <killian.meersman@outlook.com>
2014-12-22 17:47:09 +0100

    Revert "Revert "Added everything""
    This reverts commit ae12773772359907aec165985a83c8613fdf48ee.

---
+++ Trivial-Time/Controls/HexagonControl.xaml.vb
@@ -0,0 +1,23 @@
+﻿#Region "Imports"
+Imports System
+Imports System.Collections.Generic
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Animation
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+#End Region
+
+Partial Public Class HexagonControl
+    Public Sub New()
+        MyBase.New()
+
+        Me.InitializeComponent()
+
+        ' Insert code required on object creation below this point.
+    End Sub
+End Class
