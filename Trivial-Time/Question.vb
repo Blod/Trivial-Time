c0cb5bec26f4e3d042543744ccd5174a4dc8757d
Killian Meersman <killian.meersman@outlook.com>
2014-12-22 17:42:06 +0100

    Added everything

---
+++ Trivial-Time/Question.vb
@@ -0,0 +1,52 @@
+﻿Public Class Question
+
+    Private _question As String
+    Private _answers(2) As String
+    Private _correctAnswer As String
+
+    Public Property Question() As String
+        Get
+            Return _Question
+        End Get
+        Set(ByVal value As String)
+            _Question = value
+        End Set
+    End Property
+
+    Public Property Answer1() As String
+        Get
+            Return _answers(0)
+        End Get
+        Set(ByVal value As String)
+            _answers(0) = value
+        End Set
+    End Property
+
+    Public Property Answer2() As String
+        Get
+            Return _answers(1)
+        End Get
+        Set(ByVal value As String)
+            _answers(1) = value
+        End Set
+    End Property
+
+    Public Property Answer3() As String
+        Get
+            Return _answers(2)
+        End Get
+        Set(ByVal value As String)
+            _answers(2) = value
+        End Set
+    End Property
+
+    Public Property CorrectAnswer() As String
+        Get
+            Return _correctAnswer
+        End Get
+        Set(ByVal value As String)
+            _correctAnswer = value
+        End Set
+    End Property
+
+End Class
